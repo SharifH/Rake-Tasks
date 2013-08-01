874b2e6a3697a04385ba68362b33a45615d94c99
Anil Bridgpal <bridgpal@Anils-MacBook-Pro.local>
2013-08-01 10:04:56 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListDemo::Application.config.secret_token = '7560ab65bcdfc9057fec803e075d8c625c4e73b1ff01c3c03ab2845e29812bbccb7faf4b8fff8c29e58ad336f437893bc052118a1dc3340d3c5a44db9790758a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ListDemo::Application.config.session_store :cookie_store, key: '_list_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ListDemo::Application.config.session_store :active_record_store
